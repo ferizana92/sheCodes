62cfb78028ffed1eb64ed88abec4dcb93c2d5906
ferizana92 <f.motevalian92@gmail.com>
2023-05-07 13:11:47 +0430

    description of what is sheCode

---
+++ script1.js
@@ -0,0 +1,16 @@
+let city="Tehran";
+let country = "Iran";
+const sum = "I am from " +city + ",from "+country
+// ====================================================
+const userPlace = prompt("please enter your home town!");
+      alert(userPlace);
+      const userName = prompt("What is your First name?");
+      const lastName = prompt("What is your Last name?");
+alert("Your name is " + userName + lastName);
+// ====================================================
+
+
+// ====================================================
+
+
+// ====================================================
