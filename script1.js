c375e959a7a39073d330141926d3c3f9eba8b396
ferizana92 <f.motevalian92@gmail.com>
2023-05-07 15:00:57 +0430

    week2 practices

--- script1.js
+++ script1.js
@@ -8,9 +8,46 @@ const userPlace = prompt("please enter your home town!");
       const lastName = prompt("What is your Last name?");
 alert("Your name is " + userName + lastName);
 // ====================================================
+const userAge = prompt("please enter your age!", 0);
 
-
+if (userAge >= 18) {
+    alert( "you can apply to sheCodes!")
+}else {
+    alert( "you can not apply to sheCodes!")
+}
 // ====================================================
-
-
+const Time = prompt("What time is it?");
+      if (Time >= 18) {
+        alert("Good evening!🌛");
+      } else {
+        if (Time < 12) {
+          alert("Good morning!☀");
+        } else {
+          alert("Good afternoon!⭐");
+        }
+      }
+// ====================================================
+const countryName = prompt("What country are you from?");
+      if (
+        countryName === "Brazil" ||
+        "Portugal" ||
+        "BRAZIL" ||
+        "PORTUGAL" ||
+        "pORTugal"
+      ) {
+        alert("You speak Portuguese!");
+      } else {
+        alert("You don't speak Portuguese");
+      }
+// ====================================================
+const countryName1 = prompt("What country are you from?");
+countryName1 = countryName1.toLocaleLowerCase();
+countryName1 = countryName1.trim();
+countryName1 = countryName1.replace("Brasil","Brazil");
+if (countryName1 === "Portugal" || countryName1 === "Brazil"
+      ) {
+        alert("You speak Portuguese!");
+      } else {
+        alert("You don't speak Portuguese");
+      }
 // ====================================================
