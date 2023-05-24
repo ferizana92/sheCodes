129b391e2e5f782d9281ac74f019623b8cb98c27
ferizana92 <f.motevalian92@gmail.com>
2023-05-24 16:25:35 +0330

    practices for math in js

--- math/src/index.js
+++ math/src/index.js
@@ -1,1 +1,6 @@
 let price = 4.5;
+console.log(Math.round(price));
+console.log(Math.ceil(price));
+console.log(Math.floor(price));
+console.log(Math.min(1.4,5,6.1));
+console.log(Math.max(1.4,5,6.1));
