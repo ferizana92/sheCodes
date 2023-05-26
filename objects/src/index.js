fb0d8981779021e9126ad28b39a9e6af20c00839
ferizana92 <f.motevalian92@gmail.com>
2023-05-26 23:59:05 +0330

    examples

---
+++ objects/src/index.js
@@ -0,0 +1,27 @@
+// Challenge 1
+let weather = new Object(); 
+
+let temp = {
+  firsName: "Julie",
+  lastName: "Johnson",
+};
+
+let humidity = {
+  firstName: "Kate",
+  lastName: "Robinson",
+  female: true,
+};
+console.log(Object);
+// Challenge 2
+alert(weather.firstName); // Lady
+alert(weather.lastName); 
+// Challenge 3
+
+
+// Challenge 4
+
+
+
+// Challenge 5
+
+// Challenge 6
