09fe40fe26743e93447fe683ce657ad45e4d736a
ferizana92 <f.motevalian92@gmail.com>
2023-05-28 22:15:56 +0330

    objects in js

--- objects/src/index.js
+++ objects/src/index.js
@@ -1,27 +1,44 @@
-// Challenge 1
-let weather = new Object(); 
-
-let temp = {
-  firsName: "Julie",
-  lastName: "Johnson",
+// Challenge 1 and 2
+let weather = {
+  temp: 20,
+  humidity: 10
 };
+console.log(weather);
 
-let humidity = {
-  firstName: "Kate",
-  lastName: "Robinson",
-  female: true,
-};
-console.log(Object);
-// Challenge 2
-alert(weather.firstName); // Lady
-alert(weather.lastName); 
 // Challenge 3
-
+console.log(weather.temp);
+console.log(weather.humidity);
 
 // Challenge 4
-
-
+weather.windSpeed = 8.9;
+console.log(weather.windSpeed);
 
 // Challenge 5
+console.log(weather["windSpeed"]);
+console.log(weather["temp"]);
+console.log(weather["humidity"]);
 
 // Challenge 6
+let forecast = [
+  {
+    day: "Monday",
+    temp: 13
+  },
+  {
+    day: "Tuesday",
+    temp: 20
+  },
+  {
+    day: "Wednesday",
+    temp: 10
+  },
+  {
+    day: "Thursday",
+    temp: 11
+  },
+  {
+    day: "Friday",
+    temp: 20
+  }
+];
+console.log(forecast);
