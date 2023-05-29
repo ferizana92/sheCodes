38f71e7011bbc66ae2a8225739fbfade5a965b8f
ferizana92 <f.motevalian92@gmail.com>
2023-05-29 23:43:06 +0330

    data types exercise

--- data types/src/index.js
+++ data types/src/index.js
@@ -25,4 +25,19 @@ let weather = {
   }
 };
 
-// write your code here
+let city = prompt("Which city?");
+city = city.toLowerCase();
+if (weather[city] !== undefined) {
+  let temperature = weather[city].temp;
+  let humidity = weather[city].humidity;
+  let celsiusTemperature = Math.round(temperature);
+  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+
+  alert(
+    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
+  );
+} else {
+  alert(
+    `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+  );
+}
