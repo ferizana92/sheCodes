bb03d55dad6d6e8d48f129e6e1a2adaca600104c
ferizana92 <f.motevalian92@gmail.com>
2023-05-31 19:39:14 +0330

    date data

---
+++ dates/src/index.js
@@ -0,0 +1,11 @@
+// ============get current time
+let now = new Date();
+// ============get date data
+now.getMinutes(); // 0,1,2, 12
+now.getHours(); //1, 2, 3, 4
+now.getDate(); //1, 2, 3, 4
+now.getDay(); // 0, 1, 2
+now.getMonth(); // 0, 1, 2
+now.getFullYear(); // 2021
+// ============get date data
+
