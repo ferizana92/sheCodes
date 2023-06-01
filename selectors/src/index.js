d9a5180d539273f338b2bfbcb21d1daf29bd01a1
ferizana92 <f.motevalian92@gmail.com>
2023-06-01 03:30:54 +0330

    examples for query selectors

---
+++ selectors/src/index.js
@@ -0,0 +1,5 @@
+let li = document.querySelector("li");
+let day = document.querySelector(".day");
+let paragraph = document.querySelector("ul#list p");
+let lis = document.querySelectorAll("li");
+let paragraphs = document.querySelectorAll("li#special p");
