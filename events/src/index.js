600d6735600a9a800f2cc4c16abd8ec9787e52be
ferizana92 <f.motevalian92@gmail.com>
2023-06-05 16:50:34 +0330

    challenges for events

---
+++ events/src/index.js
@@ -0,0 +1,12 @@
+function sayHi() {
+  alert("hi");
+}
+
+let element = document.querySelector("#city");
+element.addEventListener("click", sayHi);
+// Challenge 1
+// Add click eventListener to the button. Alert `It is 18 degrees`
+
+
+// Challenge 2
+// When the button is clicked, replace button label by ‘18 degrees’
