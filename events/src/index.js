c04993954304d900c538349c26709ed8b33eeb84
ferizana92 <f.motevalian92@gmail.com>
2023-06-05 19:04:56 +0330

    events

--- events/src/index.js
+++ events/src/index.js
@@ -1,12 +1,18 @@
-function sayHi() {
+function changeButton() {
   alert("hi");
 }
 
-let element = document.querySelector("#city");
-element.addEventListener("click", sayHi);
+let change = document.querySelector("#temp");
+change.addEventListener("click", changeButton);
 // Challenge 1
 // Add click eventListener to the button. Alert `It is 18 degrees`
 
 
 // Challenge 2
 // When the button is clicked, replace button label by ‘18 degrees’
+function changeButton() {
+  
+}
+changeButton()
+let change = document.querySelector("#temp");
+change.addEventListener("click", changeButton);
