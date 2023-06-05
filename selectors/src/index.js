02ad0ef6cbdecad3dfa9b893b9ead0b69c784600
ferizana92 <f.motevalian92@gmail.com>
2023-06-05 16:37:15 +0330

    challenges for selectors

--- selectors/src/index.js
+++ selectors/src/index.js
@@ -1,5 +1,19 @@
-let li = document.querySelector("li");
-let day = document.querySelector(".day");
-let paragraph = document.querySelector("ul#list p");
-let lis = document.querySelectorAll("li");
-let paragraphs = document.querySelectorAll("li#special p");
+
+// Challenge 1
+// Log li with id special
+let specialCountry = document.querySelector("li#special");
+console.log(specialCountry)
+
+// Challenge 2
+// Log all li with class of country
+let countries = document.querySelectorAll("li.country");
+console.log(countries);
+
+// Challenge 3
+// Add class special to the li with id special
+specialCountry.classList.add('special');
+
+// Challenge 4
+// In your code, replace the content of the element with id special
+// by your favorite country (other than Portugal of course)
+specialCountry.innerHTML = "Thailand 🇹🇭";
