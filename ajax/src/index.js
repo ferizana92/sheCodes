3e32ce0454131c63ae737a170fb4bcf192446c25
ferizana92 <f.motevalian92@gmail.com>
2023-06-11 21:01:50 +0330

    project week5

--- ajax/src/index.js
+++ ajax/src/index.js
@@ -1,63 +1,19 @@
-function formatDate(date) {
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+let root = 'https://jsonplaceholder.typicode.com'
+let path = 'users/1'
 
-  let dayIndex = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = days[dayIndex];
+fetch(root + '/' + path)
+  .then(response => (
+    response.json()
+  ))
+  .then(json => (
+    console.log(json)
+  ));
 
-  return `${day} ${hours}:${minutes}`;
+function displayEmail() {
+  let h1 = document.querySelector("h1");
+  response.data.array.forEach(user => {
+    console.log(user.email);
+  });
+  h1.innerHTML = "hey $(response.data[0].email)";
 }
-
-function search(event) {
-  event.preventDefault();
-  let cityElement = document.querySelector("#city");
-  let cityInput = document.querySelector("#city-input");
-  cityElement.innerHTML = cityInput.value;
-}
-
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
-}
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
-}
-
-// ⏰Feature #1
-// In your project, display the current date and time using JavaScript: Tuesday 16:00
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
-
-// 🕵️‍♀️Feature #2
-// Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
-
-// 🙀Bonus Feature
-// Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit. When clicking on it, it should convert the temperature to Fahrenheit. When clicking on Celsius, it should convert it back to Celsius.
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
+  
